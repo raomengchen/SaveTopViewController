4230037506709655a994ccf0d728f999423f9362
raomengchen <raomeng915@163.com>
2018-01-05 17:03:17 +0800

    1.0.1

---
+++ SaveTopViewControllerTests/SaveTopViewControllerTests.m
@@ -0,0 +1,39 @@
+//
+//  SaveTopViewControllerTests.m
+//  SaveTopViewControllerTests
+//
+//  Created by RaoMeng on 2018/1/5.
+//  Copyright © 2018年 TianyingJiuzhou Network Technology Co. Ltd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SaveTopViewControllerTests : XCTestCase
+
+@end
+
+@implementation SaveTopViewControllerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
